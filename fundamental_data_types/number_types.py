b8d9b61a3f862e3fc233b829030e52015898443d
adeoke <adesmail100@gmail.com>
2020-04-17 12:03:52 +0100

    commented line statements

--- fundamental_data_types/number_types.py
+++ fundamental_data_types/number_types.py
@@ -10,5 +10,10 @@ print()
 print(type(2 + 4))
 print(type(2 * 4))
 print(type(2 - 4))
+
+# Remember that in storing a float more memory is required as what is before
+# the point what is after the point are saved in two different memory
+# locations.
+
 print(type(2 / 4))
 print(type(2 % 4))
