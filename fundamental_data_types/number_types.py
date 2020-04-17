929c2c24128a8599f0af406804705290aaf95904
adeoke <adesmail100@gmail.com>
2020-04-17 11:58:15 +0100

    simple print statements for interget/floats and also printing their types

---
+++ fundamental_data_types/number_types.py
@@ -0,0 +1,14 @@
+print(2 + 4)
+print(2 * 4)
+print(2 - 4)
+print(2 / 4)
+print(2 % 4)
+
+# separate statements above and below this line with a single blank line
+print()
+
+print(type(2 + 4))
+print(type(2 * 4))
+print(type(2 - 4))
+print(type(2 / 4))
+print(type(2 % 4))
