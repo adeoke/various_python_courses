1f60232234b63fb27ee806291de485214e2a9f61
adeoke <adesmail100@gmail.com>
2020-04-22 16:33:12 +0100

    regex plus capture groups examples

--- regular_expressions/regex_test.py
+++ regular_expressions/regex_test.py
@@ -21,3 +21,26 @@ print(raw_string_match2)
 
 raw_string_match3 = 'Match' if re.fullmatch('[*+$]', '*') else 'No Match'
 print(raw_string_match3)
+
+new_string = 'Very new thing, this is a simple string that contains a few words in it one repeated, this'
+
+pattern_looking_for = 'this'
+pattern = re.compile(pattern_looking_for)
+pattern_result = pattern.search(new_string)
+
+# group function returns the part of he string where there was a match
+print(pattern_result.group())
+print(len(pattern.findall(new_string)) == 2)
+
+new_pattern_looking_for = 'Very (\w+) thing'
+new_pattern = re.compile(new_pattern_looking_for)
+print(list(new_pattern.match(new_string).groups()))
+
+print(list(re.search(r'Very (new) thing', new_string).groups()))
+
+print(list(new_pattern.match(new_string).groups()) == \
+list(re.search(r'Very (new) thing', new_string).groups()))
+
+# https://regex101.com
+
+
