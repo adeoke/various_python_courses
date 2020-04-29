66a834d9cad322f10ddf73c3fd7de4d3e89d7aa3
adeoke <adesmail100@gmail.com>
2020-04-29 18:36:14 +0100

    amended soluution no more memory issues

--- codingchallengesite/repeated_string.py
+++ codingchallengesite/repeated_string.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import math
 import os
 import random
@@ -5,24 +7,28 @@ import re
 import sys
 
 
+# Complete the repeatedString function below.
 def repeated_strings(s, n):
     found_a_times = 0
-    surplus_size = math.ceil(n / len(s))
-    expanded_str = surplus_size * s
-    list_of_chars = list(expanded_str)
-    chars_width = list_of_chars[0:n]
+    additional_finds = 0
 
-    for char in chars_width:
+    for char in s:
         if char == 'a':
-            print(char)
             found_a_times += 1
 
-    return found_a_times
+    repititions_of_str = n // len(s)
+    print('repetitions of str: ', repititions_of_str)
+    no_remainder_total = repititions_of_str * found_a_times
+    remainder = n % len(s)
+    print('surplus', remainder)
+    if remainder > 0:
+        for letter in list(s)[:remainder]:
+            if letter == 'a':
+                print('theres extra of remainder:', remainder)
+                additional_finds += 1
 
+    return no_remainder_total + additional_finds
 
-# string = 'abcac'
-# letters = 10
-# repeated_strings(string, letters)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
